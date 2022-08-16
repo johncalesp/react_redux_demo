37c7c6a6fe25281526a28791ad6807d422cc8714
johncalesp <johncalesp@gmail.com>
2022-08-16 00:03:00 -0400

    install && config store

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import { CartIcon } from '../icons';
+import { useSelector } from 'react-redux';
+
+const Navbar = () => {
+  const { amount } = useSelector((store) => store.cart);
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>redux toolkit</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <div className="total-amount">{amount}</div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
